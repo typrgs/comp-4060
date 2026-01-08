17edf38b69eea7734acf8c37e3823353804947a0
Ty Paragas <ty.paragas01@gmail.com>
2026-01-07 22:27:10 -0600

    Write implementation for uart485SendBytes, add missing things to uart485Init

--- assignments/A1/drivers/uart485.c
+++ assignments/A1/drivers/uart485.c
@@ -1,4 +1,9 @@
 #include "uart485.h"
+#include "net.h"
+
+#define SENTINEL 0x7E
+#define FRAME_START 0xA5
+#define FRAME_END 0xA6
 
 uint8_t *asyncBuf = NULL;
 uart485Callback asyncDone= NULL;
@@ -6,11 +11,11 @@ uint8_t asyncSize = 0;
 
 void uart485Init(uint8_t *data, uint8_t expectedSize, uart485Callback done)
 {
-  // enable and setup generic clock 2
+  // enable and setup generic clock 5
   GCLK_REGS->GCLK_GENCTRL[5] = GCLK_GENCTRL_GENEN_Msk | GCLK_GENCTRL_SRC_DFLL | GCLK_GENCTRL_DIV(1);
   while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL_GCLK5) == GCLK_SYNCBUSY_GENCTRL_GCLK5);
 
-  // enable generic clock 2 to SERCOM0
+  // enable generic clock 5 to SERCOM0
   GCLK_REGS->GCLK_PCHCTRL[7] = GCLK_PCHCTRL_CHEN_Msk | GCLK_PCHCTRL_GEN(5);
 
   // enable main clock to SERCOM0
@@ -23,7 +28,7 @@ void uart485Init(uint8_t *data, uint8_t expectedSize, uart485Callback done)
   PORT_REGS->GROUP[0].PORT_PINCFG[8] = PORT_PINCFG_PMUXEN_Msk;
   PORT_REGS->GROUP[0].PORT_PINCFG[9] = PORT_PINCFG_PMUXEN_Msk;
 
-  // if parameters passed for async receive
+  // if parameters are passed for async receive
   if(data != NULL && expectedSize > 0 && done != NULL)
   {
     // save data buffer and callback function
@@ -33,6 +38,8 @@ void uart485Init(uint8_t *data, uint8_t expectedSize, uart485Callback done)
 
     // setup RXC interrupt
     SERCOM0_REGS->USART_INT.SERCOM_INTENSET = SERCOM_USART_INT_INTENSET_RXC_Msk;
+
+    NVIC_EnableIRQ(SERCOM0_2_IRQn);
   }
 
   // enable RX and TX
@@ -46,14 +53,65 @@ void uart485Init(uint8_t *data, uint8_t expectedSize, uart485Callback done)
     SERCOM_USART_INT_CTRLA_RXPO_PAD1 | 
     SERCOM_USART_INT_CTRLA_TXPO_PAD0;
 
+  // set baud to 0
+  SERCOM0_REGS->USART_INT.SERCOM_BAUD = 0;
+
   // enable
   SERCOM0_REGS->USART_INT.SERCOM_CTRLA |= SERCOM_USART_INT_CTRLA_ENABLE_Msk;
   while((SERCOM0_REGS->USART_INT.SERCOM_SYNCBUSY & SERCOM_USART_INT_SYNCBUSY_ENABLE_Msk) == SERCOM_USART_INT_SYNCBUSY_ENABLE_Msk);
 }
 
+void SERCOM0_2_Handler()
+{
+  // clear interrupt flag
+  SERCOM0_REGS->USART_INT.SERCOM_INTFLAG = SERCOM_USART_INT_INTFLAG_RXC_Msk;
+
+  // process RX state machine
+}
+
 void uart485SendBytes(uint8_t const * const bytes, uint16_t size)
 {
+  // wait until network is idle to start transmitting
+  // spin on "data register empty" flag
+  while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_DRE_Msk != 0);
 
+  // transmit frame start byte sequence
+  SERCOM0_REGS->USART_INT.SERCOM_DATA = SENTINEL;
+  while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_TXC_Msk != 0);
+
+  SERCOM0_REGS->USART_INT.SERCOM_DATA = FRAME_START;
+  while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_TXC_Msk != 0);
+
+  // go through byte array, sending 1 byte at a time
+  for(uint16_t i=0; i<size; i++)
+  {
+    // stuff a byte if equal to the sentinel
+    if(bytes[i] == SENTINEL)
+    {
+      SERCOM0_REGS->USART_INT.SERCOM_DATA = SENTINEL;
+      while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_TXC_Msk != 0);
+    }
+
+    SERCOM0_REGS->USART_INT.SERCOM_DATA = bytes[i];
+    while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_TXC_Msk != 0);
+  }
+
+  // if the given bytes is smaller than a packet, transmit 0x00 to fill in remaining bytes
+  if(size < PROTOCOL_PACKET_SIZE)
+  {
+    for(uint16_t i=0; i<(PROTOCOL_PACKET_SIZE - size); i++)
+    {
+      SERCOM0_REGS->USART_INT.SERCOM_DATA = 0;
+      while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_TXC_Msk != 0);
+    }
+  }
+
+  // transmit frame end byte sequence
+  SERCOM0_REGS->USART_INT.SERCOM_DATA = SENTINEL;
+  while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_TXC_Msk != 0);
+  
+  SERCOM0_REGS->USART_INT.SERCOM_DATA = FRAME_END;
+  while(SERCOM0_REGS->USART_INT.SERCOM_INTFLAG & SERCOM_USART_INT_INTFLAG_TXC_Msk != 0);
 }
 
 bool uart485ReceiveBytes(uint8_t * const bytes, uint16_t size, uint16_t timeoutMS)
