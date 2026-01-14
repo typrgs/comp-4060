e17853783912524493159bd167e4329026088984
Ty <ty.paragas01@gmail.com>
2026-01-14 11:21:05 -0600

    Add common.h

--- project/include/can.h
+++ project/include/can.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include "sam.h"
-#include <stdint.h>
-#include <stdbool.h>
+#include "common.h"
 
 // need to define message RAM area first, including rx FIFO 0 area, tx Buffers area, and extended filters area
 void canInit(uint32_t *messageRam, uint32_t *rxFifoStart, uint32_t *txBufStart, uint32_t *extendedFilterListStart, uint32_t extendedFilterListCount);
---
+++ project/include/common.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "sam.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
