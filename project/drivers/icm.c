a17ff47a8046dac8bd8b0471ebcd9934ad899af0
Ty Paragas <ty.paragas01@gmail.com>
2026-01-29 17:53:07 -0600

    Implement icmInit

--- project/drivers/icm.c
+++ project/drivers/icm.c
@@ -1,7 +1,8 @@
 #include "icm.h"
 
-static uint32_t transferDesc[4];
-static uint8_t hashArea[128];
+static icm_descriptor_registers_t transferDesc __ALIGNED(64);
+static uint32_t transferDescRegion[4]; // transfer descriptor region
+static uint8_t hashArea[32] __ALIGNED(128);
 
 void icmInit()
 {
@@ -11,9 +12,21 @@ void icmInit()
 
   // set algorithm to SHA256
   ICM_REGS->ICM_CFG = ICM_CFG_UALGO_SHA256;
+
+  // set start address of transfer descriptor
+  ICM_REGS->ICM_DSCR = (uint32_t)&transferDesc;
+
+  // set algorithm and EOM bit in transfer descriptor region
+  transferDesc.ICM_RCFG = ICM_RCFG_ALGO_Msk | ICM_RCFG_PROCDLY_SHORT | ICM_RCFG_EOM_Msk;
+
+  // set region of transfer descriptor
+  transferDesc.ICM_RADDR = (uint32_t)transferDescRegion;
+
+  // enable ICM
+  ICM_REGS->ICM_CTRL = ICM_CTRL_ENABLE_Msk;
 }
 
-static void padMsg(uint64_t msk)
+static void padMsg(uint64_t msg)
 {
 
 }
