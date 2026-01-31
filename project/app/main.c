7074778e1912451872c48bdab6c8db7b83ea8fda
Ty Paragas <ty.paragas01@gmail.com>
2026-01-30 18:29:35 -0600

    Add message ram setup to app

--- project/app/main.c
+++ project/app/main.c
@@ -1,14 +1,40 @@
 #include "heart.h"
+#include "can.h"
 #include "icm.h"
+
+#define EXTENDED_FILTER_COUNT 1
+#define RX_FIFO_ELEMENT_COUNT 1
+#define TX_BUF_ELEMENT_COUNT 1
+
+// setup message RAM for CAN
+#define EXTENDED_FILTER_SIZE EXTENDED_FILTER_COUNT * EXTENDED_FILTER_WORDS
+#define RX_FIFO_SIZE RX_FIFO_ELEMENT_COUNT * RX_FIFO_ELEMENT_WORDS
+#define TX_BUF_SIZE TX_BUF_ELEMENT_COUNT * TX_BUF_ELEMENT_WORDS
+
+uint32_t messageRAM[EXTENDED_FILTER_SIZE + RX_FIFO_SIZE + TX_BUF_SIZE] __ALIGNED(32);
+
+uint32_t *extendedFilterStart = (uint32_t *)&(messageRAM[0]);
+uint32_t *rxFifoStart = (uint32_t *)&(messageRAM[EXTENDED_FILTER_SIZE]);
+uint32_t *txBufStart = (uint32_t *)&(messageRAM[EXTENDED_FILTER_SIZE + RX_FIFO_SIZE]);
+
+uint8_t rxBuf[RX_FIFO_ELEMENT_DATA_BYTES];
+
+static void rxCallback(uint8_t len)
+{
+
+}
 
 int main()
 {
+#ifndef DNDEBUG
   for(int i=0; i<1000000; i++);
+#endif
 
   // enable interrupts
   __enable_irq();
   
   icmInit();
+  CANInit(rxFifoStart, NULL, txBufStart, extendedFilterStart, RX_FIFO_ELEMENT_COUNT, 0, TX_BUF_ELEMENT_COUNT, EXTENDED_FILTER_COUNT, rxBuf, rxCallback);
   heartInit();
 
   uint64_t msg = 0;
