6958367fe884fcfbf1358c123121ceaecffc173c
Ty Paragas <ty.paragas01@gmail.com>
2026-02-01 17:03:28 -0600

    Add helpers for tx buf and filter setup

--- project/app/main.c
+++ project/app/main.c
@@ -6,60 +6,116 @@
 
 #define EXTENDED_FILTER_COUNT 1
 #define RX_FIFO_ELEMENT_COUNT 3
-#define TX_BUF_ELEMENT_COUNT 1
+#define TX_BUF_ELEMENT_COUNT NUM_MSG_TYPES
 
 // setup message RAM for CAN
 #define EXTENDED_FILTER_SIZE EXTENDED_FILTER_COUNT * EXTENDED_FILTER_WORDS
 #define RX_FIFO_SIZE RX_FIFO_ELEMENT_COUNT * RX_FIFO_ELEMENT_WORDS
 #define TX_BUF_SIZE TX_BUF_ELEMENT_COUNT * TX_BUF_ELEMENT_WORDS
+static uint32_t messageRAM[EXTENDED_FILTER_SIZE + RX_FIFO_SIZE + TX_BUF_SIZE] __ALIGNED(32);
 
-uint32_t messageRAM[EXTENDED_FILTER_SIZE + RX_FIFO_SIZE + TX_BUF_SIZE] __ALIGNED(32);
+// setup start locations of necessary structures
+static uint32_t *extendedFilterStart = (uint32_t *)&(messageRAM[0]);
+static uint32_t *rxFifoStart = (uint32_t *)&(messageRAM[EXTENDED_FILTER_SIZE]);
+static uint32_t *txBufStart = (uint32_t *)&(messageRAM[EXTENDED_FILTER_SIZE + RX_FIFO_SIZE]);
+static uint8_t rxBuf[RX_FIFO_ELEMENT_DATA_BYTES];
 
-uint32_t *extendedFilterStart = (uint32_t *)&(messageRAM[0]);
-uint32_t *rxFifoStart = (uint32_t *)&(messageRAM[EXTENDED_FILTER_SIZE]);
-uint32_t *txBufStart = (uint32_t *)&(messageRAM[EXTENDED_FILTER_SIZE + RX_FIFO_SIZE]);
+// store a transmit buffer per message type
+static CANTxBuf txBufs[NUM_MSG_TYPES] = {0};
 
-uint8_t rxBuf[RX_FIFO_ELEMENT_DATA_BYTES];
+// store an acceptance filter per message type
+static CANExtFilter filters[NUM_MSG_TYPES] = {0};
 
-uint8_t *params = (uint8_t *)0x000fe000;
+static uint8_t deviceID;
+
 
 static void rxCallback(uint8_t len, uint32_t id)
 {
-  dbg_write_u8(rxBuf, len);
+  if(id == PULSE)
+  {
+    dbg_write_str("Pulse from: ");
+    dbg_write_u8(rxBuf, len);
+    dbg_write_char('\n');
+  }
+  else if(id == CONSENSUS)
+  {
+    dbg_write_str("Sharing blocks with ");
+    dbg_write_u8(rxBuf, len);
+    dbg_write_char('\n');
+  }
+}
+
+static void readParams()
+{
+  // save device ID given in flash parameters
+  uint8_t *params = (uint8_t *)0x000fe000;
+  deviceID = params[0];
+}
+
+static void setupFilters()
+{
+  // pulse filter
+  filters[PULSE].filterIndex = PULSE;
+  filters[PULSE].firstID = PULSE;
+  filters[PULSE].config = STF0M;
+  filters[PULSE].type = CLASSIC;
+  CANUpdateFilter(filters[PULSE]);
+
+  // consensus filter
+  filters[CONSENSUS].filterIndex = CONSENSUS;
+  filters[CONSENSUS].firstID = CONSENSUS;
+  filters[CONSENSUS].config = STF0M;
+  filters[CONSENSUS].type = CLASSIC;
+  CANUpdateFilter(filters[CONSENSUS]);
+
+  // set up all other filters to be initially disabled
+  for(MsgType i=BLOCK; i<NUM_MSG_TYPES; i++)
+  {
+    filters[i].filterIndex = i;
+    filters[i].firstID = i;
+    filters[i].config = DISABLE;
+    filters[i].type = CLASSIC;
+    CANUpdateFilter(filters[i]);
+  }
+}
+
+static void setupTxBufs()
+{
+  for(MsgType i=0; i<NUM_MSG_TYPES; i++)
+  {
+    txBufs[i].bufIndex = i;
+    txBufs[i].id = i;
+    txBufs[i].dataLength = 1;
+    txBufs[i].firstData[0] = deviceID;
+    
+    if(i == BLOCK || i == NEW_BLOCK)
+    {
+      txBufs[i].dataLength += sizeof(Block);
+    }
+
+    CANUpdateTxBuf(txBufs[i]);
+  }
 }
 
 int main()
 {
-#ifndef DNDEBUG
+#ifndef NDEBUG
   for(int i=0; i<1000000; i++);
 #endif
 
   // enable interrupts
   __enable_irq();
   
+  readParams();
   icmInit();
   CANInit(rxFifoStart, NULL, txBufStart, extendedFilterStart, RX_FIFO_ELEMENT_COUNT, 0, TX_BUF_ELEMENT_COUNT, EXTENDED_FILTER_COUNT, rxBuf, rxCallback);
   heartInit();
 
   // setup filters
-
-  // pulse filter
-  CANExtFilter filter = {0};
-  filter.filterIndex = 0;
-  filter.firstID = PULSE;
-  filter.config = STF0M;
-  filter.type = CLASSIC;
-  CANUpdateFilter(filter);
+  setupFilters();
 
   // setup transmit buffers
-
-  // pulse buffer
-  CANTxBuf buffer = {0};
-  buffer.bufIndex = 0;
-  buffer.id = PULSE;
-  buffer.dataLength = 1;
-  buffer.firstData[0] = params[0];
-  CANUpdateTxBuf(buffer);
+  setupTxBufs();
 
   // LED output
   PORT_REGS->GROUP[0].PORT_DIRSET = PORT_PA14;
@@ -73,7 +129,7 @@ int main()
     
     if(msCount >= flashTimestamp)
     {
-      CANSend(1);
+      CANSend(3);
       PORT_REGS->GROUP[0].PORT_OUTTGL = PORT_PA14;
       flashTimestamp = msCount + 500;
     }
